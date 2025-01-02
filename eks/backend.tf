63dbecf39338fb02d013c5f5cac1fe2e3ecfb471
NyerhovwoOnitcha <Pharoahpaul1@gmail.com>
2025-01-02 10:05:17 +0100

    create backend

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,3 +1,42 @@
+## create s3 bucket
+resource "aws_s3_bucket" "eks_s3" {
+  bucket = "eks-s3-terraform-bucket"
+
+  tags = {
+    Name = "statefile_bucket"
+
+  }
+}
+
+resource "aws_s3_bucket_versioning" "eks_s3_versioning" {
+  bucket = aws_s3_bucket.eks_s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+# create dynamo table
+resource "aws_dynamodb_table" "eks_s3_dynamodb-table" {
+  name         = "EksTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "eks-s3-dynamodb-table"
+  }
+}
+
+
 terraform {
   required_version = "~> 1.9.3"
   required_providers {
@@ -7,10 +46,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
+    bucket         = "eks-s3-terraform-bucket"
     region         = "eu-north-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "EksTable"
     encrypt        = true
   }
 }
